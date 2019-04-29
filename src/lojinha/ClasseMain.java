0dbefd976fb8e89819a84dee8477f7125db4464e
hobbit <nearwiner1234567@gmail.com>
2019-04-29 19:56:00 -0300

    JAVA ATUALIZÉD

--- src/lojinha/ClasseMain.java
+++ src/lojinha/ClasseMain.java
@@ -17,30 +17,26 @@ public class ClasseMain {
     
     Scanner scan = new Scanner(System.in);
     
-    System.out.println("////////////////////////Início do processo/////////////////////////");
-    
-    System.out.print("Nome do carro: ");
-    carro.nome = scan.next();
-    
-    System.out.print("Cor do carro: ");
-    carro.cor = scan.next();
-    
-    System.out.print("Motor do carro: ");
-    carro.motor = scan.next();
-    
-    System.out.print("Modelo do carro: ");
-    carro.modelo = scan.next();
-    
-    System.out.print("Com turbo?: ");
-    carro.turbo = scan.next();
-    
-    System.out.println("////////////////////////Fim do processo/////////////////////////");
-    
-    System.out.println("Nome do carro: " + carro.nome);
-    System.out.println("Cor do carro: " + carro.cor);
-    System.out.println("Motor do carro: " + carro.motor);
-    System.out.println("Modelo do carro" + carro.modelo);
-    System.out.println("Turbo: " + carro.turbo);
+        System.out.println("=================================");
+        System.out.println("Início do processo");
+        System.out.print("Nome do carro: ");
+        carro.nome = scan.next();
+        System.out.print("Cor do carro: ");
+        carro.cor = scan.next();
+        System.out.print("Motor do carro: ");
+        carro.motor = scan.next();
+        System.out.print("Modelo do carro: ");
+        carro.modelo = scan.next();
+        System.out.print("Com turbo?: ");
+        carro.turbo = scan.next();  
+        System.out.println("Fim do processo");
+        System.out.println("=================================");
+        System.out.println("Nome do carro: " + carro.nome);
+        System.out.println("Cor do carro: " + carro.cor);
+        System.out.println("Motor do carro: " + carro.motor);
+        System.out.println("Modelo do carro" + carro.modelo);
+        System.out.println("Turbo: " + carro.turbo);
+        System.out.println("==================================");
     }
     
 }
